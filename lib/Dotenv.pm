f98fe71b4761cfb5baa39e07fd9046400a46f6a3
Levi Nystad-Johansen <levi.johansen@kreftforeningen.no>
2023-04-14 13:32:14 +0200

    fix: drop pointless exporting in dotenv. fix: validate loaded config

--- lib/Dotenv.pm
+++ lib/Dotenv.pm
@@ -2,19 +2,10 @@ package Dotenv;
 use strict;
 use Carp;
 
-our (@ISA, @EXPORT, @EXPORT_OK);
-
-require Exporter; @ISA = ( 'Exporter' ); # export/import service
-
-@EXPORT = qw( Parse );
-@EXPORT_OK = qw();
-
 sub Parse {
-    my ($file) = @_;
-    $file or $file = '.env'; # default file
+    my $file = shift // '.env'; # defaulting to .env
     my %dotenv;
-    open my $fh, "<", $file
-        or croak __PACKAGE__, ": Could not open $file. $!";
+    open my $fh, "<", $file or croak __PACKAGE__, ": Could not open $file. $!";
     while(<$fh>){
         chomp;
         m{
@@ -25,15 +16,11 @@ sub Parse {
             (?<val>[^'"]+)      #value
             \k<q>               #same quote character as before
             \z
-        }xx;
-
-        croak __PACKAGE__, ": Could not parse file '$file'"
-            unless defined $+{'key'} and defined $+{'val'};
+        }x or croak __PACKAGE__, ": Could not parse file '$file'";
 
         $dotenv{$+{'key'}} = $+{'val'};
     }
-    close $fh
-        or croak __PACKAGE__, ": Could not close $file. $!";
+    close $fh;
     return %dotenv;
 }
 
